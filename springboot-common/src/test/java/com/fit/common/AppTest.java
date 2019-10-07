cecb394fe1cb8beeb1da92272a2103692ae7fecf
zhaoyong.8@163.com <Zhy@123>
2019-10-07 10:07:24 +0800

    Junit

---
+++ springboot-common/src/test/java/com/fit/common/AppTest.java
@@ -0,0 +1,19 @@
+package com.fit.common;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
--- springboot-datasource/src/main/java/com/fit/ds/DataSourceService.java
+++ springboot-datasource/src/main/java/com/fit/ds/DataSourceService.java
@@ -1,6 +1,6 @@
 package com.fit.ds;
 
-import com.fit.util.DateUtil;
+import com.fit.common.util.DateUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
