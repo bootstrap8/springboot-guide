d8833edb1219b73719625b5b93264655e7e38a7e
zhaoyong.8@163.com <Zhy@123>
2019-11-28 09:56:35 +0800

    stone

--- springboot-common/src/main/java/com/fit/common/util/JsonUtil.java
+++ springboot-common/src/main/java/com/fit/common/util/JsonUtil.java
@@ -1,6 +1,7 @@
 package com.fit.common.util;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 /**
  * json工具
@@ -15,6 +16,20 @@ public class JsonUtil {
      */
     public static String toJson(Object o) {
         return JSON.toJSONString(o);
+    }
+
+    /**
+     * json转bean
+     *
+     * @param json
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T toBean(String json, Class<T> clazz) {
+        JSONObject userJson = JSONObject.parseObject(json);
+        T object = JSON.toJavaObject(userJson, clazz);
+        return object;
     }
 
 }
