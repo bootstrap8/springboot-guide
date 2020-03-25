be2ea14492a483a1ac1a1750304a5cc3762a1df9
zhaoyong.8@163.com <Zhy@123>
2020-03-25 12:15:26 +0800

    stone

--- springboot-h2/src/main/java/com/fit/h2/user/controller/UserController.java
+++ springboot-h2/src/main/java/com/fit/h2/user/controller/UserController.java
@@ -7,10 +7,7 @@ import com.fit.h2.user.service.UserService;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Stone (300482)
@@ -33,5 +30,11 @@ public class UserController {
     @RequestMapping(value = "/addUser", method = RequestMethod.POST)
     public ResponseVo addUser(@RequestBody UserEntity user) {
         return userService.addUser(user);
+    }
+
+    @RequestMapping(value = "/queryUserByName", method = RequestMethod.POST)
+    public ResponseVo queryUserByName(@RequestParam("name") String name){
+        log.info("queryUserByName name:{}", name);
+        return ResponseVo.okFrom(name);
     }
 }
